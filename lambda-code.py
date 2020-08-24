a57ccd0687166e15a02f0f743488b6e4758ae3b1
Max Brenner <30762201+maxbren@users.noreply.github.com>
2020-08-24 14:30:50 -0400

    Create lambda-code.py

---
+++ lambda-code.py
@@ -0,0 +1,26 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    data = json.loads(json.dumps(event))
+    
+    payload = data['body']
+    
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                   ContentType='application/json',
+                                   Body=payload)
+
+    result = response['Body'].read().decode()
+
+    return {
+        'statusCode': 200,
+        'body': result
+    }
